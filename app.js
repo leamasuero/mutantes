31bcb06a9f7737ea8ff109d3432a346ad98ae351
Leandro Masuero <leandro.masuero@gmail.com>
2022-08-02 13:03:56 -0300

    mongodb atlas addon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 let port = process.env.PORT || 3000
 let mutanteService = require("./services");
 let bodyParser = require('body-parser')
+const {MongoClient, ServerApiVersion} = require('mongodb');
 
 let app = express();
 app.use(bodyParser.json())
@@ -17,6 +18,27 @@ let adn = [
     ['T', 'C', 'A', 'C', 'T', 'G'],
 ];
 
+app.get('/', function (req, res) {
+
+    const uri = "mongodb+srv://sanbox:tPGbHzCbFX5wiwpv@clusterdb.opzvo.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1
+    });
+    client.connect(err => {
+        const collection = client.db("mutantes").collection("stats");
+        console.log(collection)
+        const result = collection.insertOne({count_mutant_dna: 3})
+        console.log(result)
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        // perform actions on the collection object
+        client.close();
+    });
+
+    res.send(`${result.insertedId}`)
+})
+
 
 app.post('/mutant', function (req, res) {
 
@@ -28,7 +50,12 @@ app.post('/mutant', function (req, res) {
     })
     console.log(adn)
 
-    res.send(mutanteService.isMutante(adn, req.query.umbralMutante ?? 4));
+
+    if (mutanteService.isMutante(adn, req.query.umbralMutante ?? 4)) {
+        res.status(200).send()
+    }
+
+    res.status(403).send()
 });
 
 app.listen(port);
